216a909a4643ed4178cbfdab729ba167e489886b
TommDaniel <daninho.tomm@gmail.com>
2025-04-27 22:08:26 -0300

    change tcp client

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,7 +1,8 @@
-from PyQt5 import QtWidgets
-from ip_window import IPDialog
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from client.client import Client
+from ip_window import Ui_Dialog, IPDialog
 from client_window import Ui_MainWindow
-from repository.user import User
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -10,16 +11,20 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.actionAdd_IP.triggered.connect(self.show_ip_dialog)
         self.pushButton.clicked.connect(self.handle_search)
         self.lineEdit.setInputMask("000.000.000-00;_")
+        self.client = Client("localhost", 8080)
+        self.client.connect()
 
     def show_ip_dialog(self):
         dialog = IPDialog(self)
         if dialog.exec_() == QtWidgets.QDialog.Accepted:
-            ip = dialog.lineEdit.text()
-            port = dialog.lineEdit_2.text()
+            self.client.close()
+            self.client.set_server_ip(dialog.lineEdit.text())
+            self.client.set_server_ip(dialog.lineEdit_2.text())
+            self.client.connect()
 
     def handle_search(self):
         cpf = self.lineEdit.text()
         name = self.lineEdit_2.text()
         date = self.dateEdit.date()
-        user = User(name, cpf, date)
-        user.export_user()
+        self.client.add_requisition()
+        self.client.send_user(cpf, name, date)
--- repository/user.py
+++ repository/user.py
@@ -14,21 +14,32 @@ class User:
     def get_cpf(self):
         return self.__cpf
 
-    def export_user(self):
+    def export_user(self, request_id):
         data = {
-            "nome": self.__name,
-            "cpf": self.__cpf,
-            "data": self.__date
+            "id": request_id,
+            "data": {
+                "nome": self.__name,
+                "cpf": self.__cpf,
+                "date": self.__date
+            }
         }
-        filename = "data.json"
-        with open(filename, "w") as file:
-            json.dump(data, file)
+        data_bytes = json.dumps(data).encode('utf-8')
+        return data_bytes
 
-    def import_user(self):
-        filename = "data.json"
-        with open(filename, "r") as file:
-            data = json.load(file)
-            self.__name = data["nome"]
-            self.__cpf = data["cpf"]
-            self.__date = data["data"]
+    def import_user(self, data_bytes):
+        data = json.loads(data_bytes.decode('utf-8'))
+        request_id = data["id"]
+        user_data = data["date"]
+        self.__name = user_data["nome"]
+        self.__cpf = user_data["cpf"]
+        self.__date = user_data["data"]
 
+        data = {
+            "id": request_id,
+            "data": {
+                "nome": self.__name,
+                "cpf": self.__cpf,
+                "date": self.__date
+            }
+        }
+        return data
